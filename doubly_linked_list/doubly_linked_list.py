6256358c62c0424cb42c46034e65832430e6b1a9
JWNicholson <john.nicholson812@gmail.com>
2020-07-14 17:08:07 -0400

    remove_from_head and add_to_tail working

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -2,76 +2,112 @@
 Each ListNode holds a reference to its previous node
 as well as its next node in the List.
 """
+
+
 class ListNode:
     def __init__(self, value, prev=None, next=None):
         self.prev = prev
         self.value = value
         self.next = next
-            
+
+
 """
 Our doubly-linked list class. It holds references to 
 the list's head and tail nodes.
 """
+
+
 class DoublyLinkedList:
     def __init__(self, node=None):
         self.head = node
         self.tail = node
-        self.length = 1 if node is not None else 0
 
+#initialize list length
     def __len__(self):
-        return self.length
-    
+        current = self.head
+        length = 0
+        if current:
+            length += 1
+            while current.next is not None:
+                length += 1
+                current.prev = current
+                current = current.next
+        return length
+
     """
     Wraps the given value in a ListNode and inserts it 
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly.
     """
+
     def add_to_head(self, value):
-       self.length += 1
-       if not self.head and not self.tail:
-           # head and tail empty list
-           self.head = self.tail = ListNode(value)
-       else:
-           #list is not empty
-           self.head.insert_before(value)
-           self.head = self.head.prev
-      
-        
+        #create an instance of ListNode() with value
+        new_node = ListNode(value, None, self.head)
+        current = self.head
+        #Empty
+        if not current:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            # not empty
+            self.head = new_node
     """
     Removes the List's current head node, making the
     current head's next node the new head of the List.
     Returns the value of the removed Node.
     """
+
     def remove_from_head(self):
-        pass
-            
+        current = self.head
+        if not current:
+            return None
+        elif not current.prev:
+            result = self.head
+            self.head = self.head.next
+            self.tail = self.tail.prev
+            return result.value
+        else:
+            result = self.head
+            self.head = self.head.next
+            return result.value
+
     """
     Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly.
     """
+
     def add_to_tail(self, value):
-        pass
-            
+        new_node = ListNode(value, self.tail)
+        current = self.head
+        if not current:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
     """
     Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node.
     """
+
     def remove_from_tail(self):
         pass
-            
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List.
     """
+
     def move_to_front(self, node):
         pass
-        
+
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
+
     def move_to_end(self, node):
         pass
 
@@ -79,6 +115,7 @@ class DoublyLinkedList:
     Deletes the input node from the List, preserving the 
     order of the other elements of the List.
     """
+
     def delete(self, node):
         pass
 
@@ -86,5 +123,10 @@ class DoublyLinkedList:
     Finds and returns the maximum value of all the nodes 
     in the List.
     """
+
     def get_max(self):
         pass
+
+
+
+
